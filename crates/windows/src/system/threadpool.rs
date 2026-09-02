c74465e1eac931f3389d9c18c88d6459d7329ac5
recregt <203110092+recregt@users.noreply.github.com>
2026-09-03 01:25:56 +0300

    fix: don't catch_unwind on the native pool thread

--- crates/windows/src/system/threadpool.rs
+++ crates/windows/src/system/threadpool.rs
@@ -1,5 +1,4 @@
 use std::ffi::c_void;
-use std::panic::{AssertUnwindSafe, catch_unwind};
 
 use windows::Win32::System::Threading::{PTP_CALLBACK_INSTANCE, TrySubmitThreadpoolCallback};
 
@@ -8,7 +7,7 @@ unsafe extern "system" fn run_closure<F: FnOnce() + Send>(
     context: *mut c_void,
 ) {
     let f = *unsafe { Box::from_raw(context as *mut F) };
-    let _ = catch_unwind(AssertUnwindSafe(f));
+    f();
 }
 
 pub fn spawn_on_threadpool<F: FnOnce() + Send + 'static>(f: F) -> bool {
@@ -50,21 +49,5 @@ mod tests {
             let _ = tx.send(2 + 2);
         }));
         assert_eq!(rx.recv_timeout(Duration::from_secs(2)).unwrap(), 4);
-    }
-
-    #[test]
-    fn a_panicking_closure_does_not_abort_the_process() {
-        assert!(spawn_on_threadpool(|| {
-            panic!("deliberate panic to prove the pool survives it");
-        }));
-
-        let (tx, rx) = mpsc::channel();
-        assert!(spawn_on_threadpool(move || {
-            let _ = tx.send(());
-        }));
-        assert!(
-            rx.recv_timeout(Duration::from_secs(2)).is_ok(),
-            "the process (and this test) must still be alive after the panic to reach this point"
-        );
     }
 }
